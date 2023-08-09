4c4d1b27c8eb701a20285b1d6576f967fe110660
peterhnm <peter.hnm@gmx.de>
2023-08-09 16:05:37 +0200

    chore: fix vite config

--- src/web/vite.config.js
+++ src/web/vite.config.js
@@ -2,42 +2,48 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
-export default defineConfig({
-    resolve: {
-        alias: [
-            {
-                find: "@",
-                replacement: path.resolve(__dirname, "./app"),
-            },
+export default defineConfig(({ command }) => {
+    let root = "";
+    if (command === "build") {
+        root = "src/web";
+    }
+    return {
+        resolve: {
+            alias: [
+                {
+                    find: "@",
+                    replacement: path.resolve(__dirname, "./app"),
+                },
+            ],
+        },
+        plugins: [
+            vue({
+                template: {
+                    compilerOptions: {
+                        // treat all tags with a dash as custom elements
+                        isCustomElement: (tag) => tag.includes("-"),
+                    },
+                },
+            }),
         ],
-    },
-    plugins: [
-        vue({
-            template: {
-                compilerOptions: {
-                    // treat all tags with a dash as custom elements
-                    isCustomElement: (tag) => tag.includes("-"),
+        build: {
+            target: "es2021",
+            commonjsOptions: {
+                transformMixedEsModules: true,
+            },
+            outDir: "dist/client",
+            rollupOptions: {
+                input: `${root}/app/main.ts`,
+                output: {
+                    // don't hash the name of the output file (index.js)
+                    entryFileNames: `[name].js`,
+                    assetFileNames: `[name].[ext]`,
                 },
             },
-        }),
-    ],
-    build: {
-        target: "es2021",
-        commonjsOptions: {
-            transformMixedEsModules: true,
         },
-        outDir: "dist/client",
-        rollupOptions: {
-            input: "src/web/app/main.ts",
-            output: {
-                // don't hash the name of the output file (index.js)
-                entryFileNames: `[name].js`,
-                assetFileNames: `[name].[ext]`,
-            },
+        define: {
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
         },
-    },
-    define: {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-    },
+    };
 });
