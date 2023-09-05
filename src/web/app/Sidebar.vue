c9ddfbcfa6422f7d308437d52a6141ed0df6f93a
MichaelWildermann <michael.wildermann1@hs-augsburg.de>
2023-09-05 15:24:20 +0200

    change position button

--- src/web/app/Sidebar.vue
+++ src/web/app/Sidebar.vue
@@ -1,50 +1,43 @@
 <template>
-    <div>
-      <button @click="toggleSidebar">Toggle Sidebar</button>
-      <div id="sidebar" v-show="isSidebarVisible">
-        <!-- Sidebar content -->
-        <p>Here is some sample content for the sidebar.</p>
-      </div>
+    <div class="sidebar-container">
+        <button :style="buttonStyle" @click="toggleSidebar">
+            <div class="hamburger-icon">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div id="sidebar" :class="{ show: isSidebarVisible }">
+            <!-- Sidebar content -->
+            <p>Here is some sample content for the sidebar.</p>
+        </div>
     </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, ref } from 'vue';
-  import "./css/style.css";
-  
-  export default defineComponent({
-    name: 'Sidebar',
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from "vue";
+import "./css/style.css";
+
+export default defineComponent({
+    name: "Sidebar",
     setup() {
-      const isSidebarVisible = ref(false);
-  
-      const toggleSidebar = () => {
-        isSidebarVisible.value = !isSidebarVisible.value;
-      };
-  
-      return {
-        isSidebarVisible,
-        toggleSidebar
-      };
-    }
-  });
-  </script>
-  
-  <style scoped>
-  #sidebar {
-      width: 33%;
-      position: fixed;
-      top: 0;
-      left: 0;
-      bottom: 0;
-      background-color: #f9f9f9;
-      overflow-y: auto;
-      transition: transform 0.3s ease-in-out;
-      transform: translateX(-100%);
-      padding: 1rem;
-  }
-  
-  #sidebar.show {
-      transform: translateX(0);
-  }
-  </style>
-  
+        const isSidebarVisible = ref(false);
+
+        const toggleSidebar = () => {
+            isSidebarVisible.value = !isSidebarVisible.value;
+        };
+
+        const buttonStyle = computed(() => {
+            return {
+                left: isSidebarVisible.value ? "33%" : "0",
+            };
+        });
+
+        return {
+            isSidebarVisible,
+            toggleSidebar,
+            buttonStyle,
+        };
+    },
+});
+</script>
