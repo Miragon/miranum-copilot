234537f64bac933e717d8f3b248b23b1a8e65088
MichaelWildermann <michael.wildermann1@hs-augsburg.de>
2023-09-18 23:51:01 +0200

    add sidebar content

--- src/web/app/Sidebar.vue
+++ src/web/app/Sidebar.vue
@@ -9,7 +9,23 @@
         </button>
         <div id="sidebar" :class="{ show: isSidebarVisible }">
             <!-- Sidebar content -->
-            <p>Here is some sample content for the sidebar.</p>
+            <ul>
+                <li
+                    v-for="category in categories"
+                    :key="category.name"
+                    @click="selectCategory(category.name)"
+                >
+                    {{ category.name }}
+                    <span class="expand-icon">{{
+                        selectedCategory === category.name ? "▼" : "▶"
+                    }}</span>
+                    <ul v-if="selectedCategory === category.name">
+                        <li v-for="prompt in category.prompts" :key="prompt">
+                            {{ prompt }}
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
@@ -22,6 +38,33 @@ export default defineComponent({
     name: "Sidebar",
     setup(props, { emit }) {
         const isSidebarVisible = ref(false);
+        const selectedCategory = ref("");
+        const categories = ref([
+            {
+                name: "Category 1",
+                prompts: ["Prompt 1.1", "Prompt 1.2", "Prompt 1.3", "Prompt 1.4"],
+            },
+            {
+                name: "Category 2",
+                prompts: ["Prompt 2.1", "Prompt 2.2", "Prompt 2.3", "Prompt 2.4"],
+            },
+            {
+                name: "Category 3",
+                prompts: ["Prompt 3.1", "Prompt 3.2", "Prompt 3.3", "Prompt 3.4"],
+            },
+            {
+                name: "Category 4",
+                prompts: ["Prompt 4.1", "Prompt 4.2", "Prompt 4.3", "Prompt 4.4"],
+            },
+        ]);
+
+        const selectCategory = (categoryName: string) => {
+            if (selectedCategory.value === categoryName) {
+                selectedCategory.value = "";
+            } else {
+                selectedCategory.value = categoryName;
+            }
+        };
 
         const toggleSidebar = () => {
             isSidebarVisible.value = !isSidebarVisible.value;
@@ -38,6 +81,9 @@ export default defineComponent({
             isSidebarVisible,
             toggleSidebar,
             buttonStyle,
+            categories,
+            selectedCategory,
+            selectCategory,
         };
     },
 });
