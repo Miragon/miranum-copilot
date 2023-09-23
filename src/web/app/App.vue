808951ab5f53bc5a6ce419c94be16314eee5c1a4
Peter Heinemann <peter.heinemann@miragon.io>
2023-09-23 10:16:16 +0200

    feat(frontend): add loading animation (#105)

--- src/web/app/App.vue
+++ src/web/app/App.vue
@@ -13,6 +13,7 @@ import {
     VsCode,
 } from "@/composables";
 
+import "./css/vscode-theme-colors.css"; // remove this for production
 import "./css/style.css";
 
 // provideVSCodeDesignSystem().register(allComponents);
@@ -30,6 +31,8 @@ if (process.env.NODE_ENV === "development") {
 } else {
     stateController = new StateController();
 }
+
+const loading = ref(true);
 
 let inputText = ref("");
 let outputText = ref("");
@@ -90,6 +93,7 @@ onBeforeMount(async () => {
 function postMessage(type: MessageType, data?: string, info?: string): void {
     switch (type) {
         case MessageType.msgFromWebview: {
+            loading.value = true;
             stateController.postMessage({
                 type: `${globalViewType}.${type}`,
                 data: data ? data : "",
@@ -119,6 +123,7 @@ function receiveMessage(message: MessageEvent<VscMessage<string>>): void {
 
         switch (type) {
             case `${globalViewType}.${MessageType.initialize}`: {
+                loading.value = false;
                 resolver.done(data);
                 break;
             }
@@ -127,6 +132,7 @@ function receiveMessage(message: MessageEvent<VscMessage<string>>): void {
                 break;
             }
             case `${globalViewType}.${MessageType.msgFromExtension}`: {
+                loading.value = false;
                 outputText.value = data ? data : "";
                 break;
             }
@@ -140,42 +146,66 @@ function receiveMessage(message: MessageEvent<VscMessage<string>>): void {
 
 <template>
     <main>
-        <div class="input-container">
-            <vscode-text-area
-                id="inputText"
-                v-model="inputText"
-                cols="40"
-                maxlength="1000"
-                placeholder="Enter your prompt here"
-                resize="vertical"
-                rows="10"
-            >
-                Your question:
-            </vscode-text-area>
-            <vscode-button
-                id="submitButton"
-                @click="postMessage(MessageType.msgFromWebview, inputText)"
-            >
-                Send Prompt
-            </vscode-button>
+      <div class="input">
+          <vscode-text-area
+              id="inputText"
+              v-model="inputText"
+              cols="40"
+              maxlength="1000"
+              placeholder="Enter your prompt here"
+              resize="vertical"
+              rows="10"
+          >
+            Your question:
+          </vscode-text-area>
+          <vscode-button
+              id="submitButton"
+              appearance="primary"
+              aria-label="Send Request"
+              @click="postMessage(MessageType.msgFromWebview, inputText)"
+          >
+            Send Prompt
+          </vscode-button>
+      </div>
+
+      <div class="output">
+        <div v-if="loading" class="output-loading">
+          <span class="loader"></span>
         </div>
-        <div class="output-container">
-            <vscode-text-area
-                id="outputText"
-                v-model="outputText"
-                cols="60"
-                placeholder="Your answer will be printed here"
-                readonly
-                rows="15"
-            >
-                Response from ChatGPT
-            </vscode-text-area>
+
+        <div v-if="!loading" class="output-loaded">
+          <vscode-text-area
+              id="outputText"
+              v-model="outputText"
+              cols="60"
+              placeholder="Your answer will be printed here"
+              readonly
+              rows="15"
+          >
+            Response from ChatGPT
+          </vscode-text-area>
         </div>
+      </div>
+
     </main>
 </template>
 
 <style scoped>
+main {
+  display: grid;
+  grid-template-areas:
+      "input"
+      "output";
+  gap: 40px;
+
+  width: 800px;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
 vscode-text-area {
+    min-height: inherit;
     width: 100%;
     margin-bottom: var(--margin);
     padding: 10px;
@@ -187,5 +217,73 @@ vscode-button {
     width: 100%;
     padding: 10px;
     transition: background-color 0.3s ease;
+}
+
+.input {
+  grid-area: input;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+}
+
+.output {
+  grid-area: output;
+  min-height: 400px;
+}
+
+.output-loading {
+  display: grid;
+  min-height: inherit;
+  align-content: center;
+  justify-content: center;
+}
+
+.output-loaded {
+  min-height: inherit;
+}
+
+/* Loader Animation*/
+.loader {
+  display: inline-block;
+  position: relative;
+  width: 48px;
+  height: 40px;
+  margin-top: 30px;
+  background: var(--vscode-button-background);
+  border-radius: 15% 15% 35% 35%;
+}
+.loader::after {
+  content: '';
+  box-sizing: border-box;
+  position: absolute;
+  left: 45px;
+  top: 8px;
+  border: 4px solid var(--vscode-button-background);
+  width: 16px;
+  height: 20px;
+  border-radius: 0 4px 4px 0;
+}
+.loader::before {
+  content: '';
+  position: absolute;
+  width: 1px;
+  height: 10px;
+  color: var(--vscode-editor-foreground);
+  top: -15px;
+  left: 11px;
+  box-sizing: border-box;
+  animation: animloader 1s ease infinite;
+}
+
+@keyframes animloader {
+  0% {
+    box-shadow: 2px 0px rgba(255, 255, 255, 0), 12px 0px rgba(255, 255, 255, 0.3), 20px 0px rgba(255, 255, 255, 0);
+  }
+  50% {
+    box-shadow: 2px -5px rgba(255, 255, 255, 0.5), 12px -3px rgba(255, 255, 255, 0.5), 20px -2px rgba(255, 255, 255, 0.6);
+  }
+  100% {
+    box-shadow: 2px -8px rgba(255, 255, 255, 0), 12px -5px rgba(255, 255, 255, 0), 20px -5px rgba(255, 255, 255, 0);
+  }
 }
 </style>
