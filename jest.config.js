59f90062b3bd074f61cd2916d4994b251f7224d3
peterhnm <peter.hnm@gmx.de>
2023-10-05 22:41:55 +0200

    feat: switch from jest to mocha

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("ts-jest").JestConfigWithTsJest} */
-module.exports = {
-    collectCoverage: true,
-    coverageDirectory: "<rootDir>/coverage",
-    coveragePathIgnorePatterns: ["dist", "<rootDir>/src/test"],
-    testPathIgnorePatterns: ["dist", "<rootDir>/src/test"],
-    preset: "ts-jest",
-    testEnvironment: "node",
-};
