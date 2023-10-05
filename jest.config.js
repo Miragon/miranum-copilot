a9ca8a8dfd8085b767233b44a7b4f8303072efd0
peterhnm <peter.hnm@gmx.de>
2023-10-05 14:12:28 +0200

    chore(deps): upgrade openai

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+/** @type {import("ts-jest").JestConfigWithTsJest} */
 module.exports = {
+    collectCoverage: true,
+    coverageDirectory: "<rootDir>/coverage",
+    coveragePathIgnorePatterns: ["dist", "<rootDir>/src/test"],
+    testPathIgnorePatterns: ["dist", "<rootDir>/src/test"],
     preset: "ts-jest",
     testEnvironment: "node",
 };
